bcbba0964800caa355087f3d47492a77407cddc1
simasnau <simasnau@gmail.com>
2021-11-03 21:11:15 +0200

    Initialized project

---
+++ LaboratorinisDarbas3/src/test/java/lt/vu/laboratorinisdarbas3/LaboratorinisDarbas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vu.laboratorinisdarbas3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorinisDarbas3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
